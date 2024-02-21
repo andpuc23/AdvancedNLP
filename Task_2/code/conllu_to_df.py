e09316a00a74bbae5f7fe0cad9d3dc9281e1c040
andpuc23 <44880821+andpuc23@users.noreply.github.com>
2024-02-21 11:14:57 +0100

    Add voice feture to extract_features

---
+++ Task_2/code/conllu_to_df.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+def process_file(conll_file, output_csv):
+
+# conll_file = '../UP-1.0/en_ewt-up-dev.conllu'
+  df = pd.DataFrame()
+  sentences = []
+  all_df = []
+  with open(conll_file, 'r', encoding="utf8") as file:
+    data = file.read()
+    sentences = data.strip().split('\n\n')
+
+  all_df = []
+  for sentence in sentences:
+    rows = sentence.strip().split('\n')
+    rows = [row for row in rows if not row[0].startswith('#')]
+    columns = [row.split('\t') for row in rows]
+    if len(columns[0])>10:
+      df = pd.DataFrame(columns)  
+      num_predicates = df[10].apply(lambda x: 1 if x !='_' else 0).sum()
+
+      if num_predicates == df.shape[1]-11:
+          for j in range(num_predicates):
+              df_new = df.iloc[:,[0,1,2,3,4,5,6,7,8,9,10,10+j+1]]
+              new_names = {col: k for k, col in enumerate(df_new.columns)}
+              df_new = df_new.rename(columns=new_names)
+              all_df.append(df_new)
+
+  big_df = pd.concat(all_df, ignore_index=True)
+  print(len(big_df))
+  # output_csv = '../UP-1.0/output/en_ewt-up-dev.csv'
+  big_df.to_csv(output_csv)
