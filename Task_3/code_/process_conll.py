ae249e2e5b5fdbdab0c7e3d7e8b6435eedf36a90
andpuc23 <44880821+andpuc23@users.noreply.github.com>
2024-02-28 18:13:33 +0100

    Sentences extraction, label extraction is todo

--- Task_3/code_/process_conll.py
+++ Task_3/code_/process_conll.py
@@ -1,36 +1,45 @@
 # here we (will) have both reading of conllu and features extraction
 import pandas as pd
 
+def _get_predicates_from_sentence(lines):
+    # here we expect that sentence is a number of lines
+    pred_positions = []
+    for line in lines[2:]:
+        if any([c == 'V' for c in line.split('\t')[11:]]):
+            pred_positions.append(int(line.split('\t')[0])-1)
+    return pred_positions
+
+
+
 def process_file(conll_file)->pd.DataFrame:
-    df = pd.DataFrame()
-    sentences = []
-    all_df = []
-    with open(conll_file, 'r', encoding="utf8") as file:
-        data = file.read()
-        sentences = data.strip().split('\n\n')
+    big_df = pd.DataFrame(columns=['sentence', 'predicate', 'predicate index'])
+    with open(conll_file) as f:
+        text = f.read()
+    
+    sentences = text.split('\n\n')  # split by empty line - sent id+text+table with features
+    
+    for s in sentences:
+        lines = s.split('\n')
+        if lines[0].startswith('# propbank'):
+            lines = lines[1:]
+        if lines[0].startswith('# newdoc'):
+            lines = lines[1:]
+        
+        if len(lines) > 1:
+            sentence_text = lines[1][len('# text = '):]  # everything after "# text = ""
+        
+            pred_idxs = _get_predicates_from_sentence(lines)
+            for idx in pred_idxs:
+                word = lines[idx+2].split('\t')[1]
+                big_df.loc[len(big_df.index)] = [sentence_text, word, idx]
 
-    all_df = []
-    for sentence in sentences:
-        rows = sentence.strip().split('\n')
-        rows = [row for row in rows if not row[0].startswith('#')]
-        columns = [row.split('\t') for row in rows]
-        if len(columns[0])>10:
-            df = pd.DataFrame(columns)  
-            num_predicates = df[10].apply(lambda x: 1 if x !='_' else 0).sum()
-
-            if num_predicates == df.shape[1]-11:
-                for j in range(num_predicates):
-                    df_new = df.iloc[:,[0,1,2,3,4,5,6,7,8,9,10,10+j+1]]
-                    new_names = {col: k for k, col in enumerate(df_new.columns)}
-                    df_new = df_new.rename(columns=new_names)
-                    all_df.append(df_new)
-
-    big_df = pd.concat(all_df, ignore_index=True)
     print('process_file(): dataframe len:', len(big_df))
     return big_df
-    # big_df.to_csv(output_csv, index=False, header=False)
 
 
 def extract_features(dataframe)->pd.DataFrame:
-    new_df = dataframe
-    return new_df
+    df = pd.DataFrame(columns=['sentences', 'labels'])
+    df.sentences = [a + '[SEP]' + b for a, b in zip(dataframe['sentence'].values, dataframe['predicate'].values)]
+    # for now I put here the index of word, but it should be the label we predict
+    df.labels = dataframe['predicate index']
+    return df
